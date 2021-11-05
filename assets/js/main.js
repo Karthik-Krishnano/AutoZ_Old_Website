af7d7871737d281d9907203c348c9ef744917ba1
Karthik-Krishnano <73122483+Karthik-Krishnano@users.noreply.github.com>
2021-11-06 03:28:59 +0530

    first stage completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -177,6 +177,25 @@
     });
   }
 
+  // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
   /**
    * Clients Slider
    */
@@ -284,6 +303,8 @@
       clickable: true
     }
   });
+
+
 
   /**
    * Animation on scroll
